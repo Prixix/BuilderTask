935a24544d87421f90e459520c5f89d65e35cd27
Paul Artjomow <24830552+Prixix@users.noreply.github.com>
2021-03-13 13:25:23 +0100

    Added basic URL version control on startup

--- src/main/java/de/prixix/buildertask/BuilderTask.java
+++ src/main/java/de/prixix/buildertask/BuilderTask.java
@@ -2,11 +2,27 @@ package de.prixix.buildertask;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public final class BuilderTask extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        try {
+            URL versionURL = new URL("");
+            URLConnection urlConnection = versionURL.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+            String inputLine = in.readLine();
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
