86f2b8853764d217ffb13993a53b509a4bbdf9cc
Paul Artjomow <24830552+Prixix@users.noreply.github.com>
2021-03-13 12:57:14 +0100

    Initial commit

---
+++ src/main/java/de/prixix/buildertask/BuilderTask.java
@@ -0,0 +1,17 @@
+package de.prixix.buildertask;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BuilderTask extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
